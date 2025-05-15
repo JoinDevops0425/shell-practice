4e4586def20dd8072a20ed00924398883b067765
JoinDevops0425 <joindevops0425@gmail.com>
2025-05-15 22:51:14 +0530

    m

--- conversation.sh
+++ conversation.sh
@@ -1,7 +1,7 @@
 #!bin?bash
 
-PERSON1=Modi #there should be no space around the equal sign
-PERSON2=MUSK
+PERSON1= #there should be no space around the equal sign
+PERSON2=
 
 echo "$PERSON1 is a great president"
 echo "$PERSON2 is a great president"
