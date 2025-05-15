6c8b5ae0a140b2fc8d0584142bb9adc77e895ba1
JoinDevops0425 <joindevops0425@gmail.com>
2025-05-15 23:07:45 +0530

    Uday

--- array.sh
+++ array.sh
@@ -4,4 +4,6 @@ MOVIES=("court" "HIt" "Ihaveno idea")
 
 echo "the first movies is ${MOVIES[0]}"
 
-echo "all movies${MOVIES[@]}
+echo "all movies${MOVIES[@]}"
+echo "all movies${MOVIES[%]}"
+echo "all movies${MOVIES[*]}"
