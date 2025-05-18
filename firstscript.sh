2a0650e97b74325cd8a4b5f2a41a1a1fc2938a6e
JoinDevops0425 <joindevops0425@gmail.com>
2025-05-18 22:39:36 +0530

    conditions

---
+++ firstscript.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then 
+    echo "Error :: PLease run with root access"
+    exit 1
+else 
+    echo "You are root user"
+fi
+
+dnf install mysql -y
