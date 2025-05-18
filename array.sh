fdd7964736b36bd68d08901e1fdbe0f22f63523d
JoinDevops0425 <joindevops0425@gmail.com>
2025-05-18 19:17:25 +0530

    another program with Data Types

--- array.sh
+++ array.sh
@@ -6,6 +6,7 @@ echo "First Movies is : ${MOVIES[0]}"
 echo "First Movies is : ${MOVIES[2]}"
 echo "First Movies is : ${MOVIES[*]}"
 echo "First Movies is : ${MOVIES[@]}"
+echo "First Movies is : ${MOVIES[3]}"
 
 
 
