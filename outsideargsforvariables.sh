ae8df92c538c88c9a1587ac5f9508cfd030e0f17
JoinDevops0425 <joindevops0425@gmail.com>
2025-05-18 18:58:57 +0530

    Outside args imout

---
+++ outsideargsforvariables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# This script is used to create a conversation with the user
+PERSON1=$1
+PERSON2=$2
+
+
+echo "$PERSON1 : Hey $PERSON2, How are you?"
+echo "$PERSON2 : I am Good, How are you?"
+echo "$PERSON1 : I am good too, what's up?"
+echo "$PERSON2 : I am going to Mars, WOuld you like to Join?"
+echo "$PERSON1 : Sure, I would love to join you."
+echo "$PERSON2 : Let's go then"
